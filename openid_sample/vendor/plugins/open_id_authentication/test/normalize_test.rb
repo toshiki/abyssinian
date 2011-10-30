19d5c9e184b6a0f6506651e45113f9851ce094b0
Gun.io Whitespace Robot <contact@gun.io>
2011-10-29 20:55:36 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- openid_sample/vendor/plugins/open_id_authentication/test/normalize_test.rb
+++ openid_sample/vendor/plugins/open_id_authentication/test/normalize_test.rb
@@ -24,7 +24,7 @@ class NormalizeTest < Test::Unit::TestCase
       assert_equal to, normalize_url(from)
     end
   end
-  
+
   def test_broken_open_id
     assert_raises(InvalidOpenId) { normalize_url(nil) }
     assert_raises(InvalidOpenId) { normalize_url("=name") }
